749b54185be61971c5c301642640012f99f55152
BugHunter-X <askzenai@gmail.com>
2024-12-26 03:10:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+function foo(a,b){    if(a === 0 || b === 0){        return 0;    }    return a/b;}console.log(foo(1,0));//Output: Infinityconsole.log(foo(0,1));//Output: 0console.log(foo(0,0));//Output: 0
